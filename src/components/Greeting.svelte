698ac1d85064ab8454b3746c97928db9d684f64a
Alvaro Fernandez <alvaroofernaandez@gmail.com>
2024-07-28 20:41:14 +0200

    componente Svelte añadido

---
+++ src/components/Greeting.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  const currentTime = new Date()
+  const currentHour = currentTime.getHours()
+
+  let greeting = ""
+
+  if(currentHour > 12) {
+    greeting = "Buenos días, ¡Bienvenido a HaganeFy!"
+  } else if (currentHour < 18) {
+    greeting = "Buenas tardes, ¡Bienvenido a HaganeFy!"
+  } else {
+    greeting = "Buenas noches, ¡Bienvenido a HaganeFy!"
+  }
+</script>
+
+<h1 class="text-3xl font-bold">{greeting}</h1>
